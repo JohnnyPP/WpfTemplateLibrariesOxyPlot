0d3b3602075fab589c4eb5184ed7a8cbfb1606e6
Johnny P <johnnnyppp@gmail.com>
2016-05-05 18:14:28 +0200

    Moved data to model

--- WpfModel/Model.cs
+++ WpfModel/Model.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace WpfModel
 {
@@ -40,6 +42,32 @@ namespace WpfModel
         {
             Click++;
         }
+
+        public List<LineSeries> ModelPlotData()
+        {
+            var lineSeriesList = new List<LineSeries>();
+
+            // Create two line series (markers are hidden by default)
+            var series1 = new LineSeries { Title = "Series 1", MarkerType = MarkerType.Circle };
+            series1.Points.Add(new DataPoint(0, 0));
+            series1.Points.Add(new DataPoint(10, 18));
+            series1.Points.Add(new DataPoint(20, 12));
+            series1.Points.Add(new DataPoint(30, 8));
+            series1.Points.Add(new DataPoint(40, 15));
+
+            var series2 = new LineSeries { Title = "Series 2", MarkerType = MarkerType.Square };
+            series2.Points.Add(new DataPoint(0, 4));
+            series2.Points.Add(new DataPoint(10, 12));
+            series2.Points.Add(new DataPoint(20, 16));
+            series2.Points.Add(new DataPoint(30, 25));
+            series2.Points.Add(new DataPoint(40, 5));
+
+            lineSeriesList.Add(series1);
+            lineSeriesList.Add(series2);
+
+            return lineSeriesList; 
+        }
+
         #endregion
 
         #region INotifyPropertyChanged implementation
